14e79d192f6ede0f6645014cf32298eb6db2713a
Sultan <m.sultan338@gmail.com>
2021-04-22 01:13:12 +0500

    add component 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { component, Component } from "react";
+import Component_1 from "./components/Component_1";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Welcome</h1>
+        <Component_1 />
       </div>
     );
   }
---
+++ src/components/Component_1.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Component_1() {
+  return (
+    <div style={c1Style}>
+      <h1>Component 1</h1>
+      <p>What a component 1</p>
+    </div>
+  );
+}
+
+const c1Style = {
+  background: "steelblue",
+  color: "white",
+  padding: "1.5rem",
+};
